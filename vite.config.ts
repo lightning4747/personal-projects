ba03b077dea4a26026bbdbb78b45b1f4d4d50cfb
lightning4747 <vignesh112847@gmail.com>
2026-02-10 22:38:33 +0530

    add redirects

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === 'production' ? 'https://personal-projects-venv.netlify.app/' : '/',
-}))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    base: env.VITE_APP_URL || '/',
+  }
+})
