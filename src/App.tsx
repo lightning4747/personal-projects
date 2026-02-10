b374acbee64d0d2f3c81dd5bd11e6d3eeaaaae03
lightning4747 <vignesh112847@gmail.com>
2026-02-10 21:53:38 +0530

    Create a separate folder for the todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import TodoPage from './pages/todo';
+import TodoPage from './pages/todo/Todo';
 
 function App() {
   return (
